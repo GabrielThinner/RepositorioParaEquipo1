5df39e1c7b741731b42b206f244d5b52f37814c5
Gabriel Alejandro Flores Gutierrez <142535427+GabrielThinner@users.noreply.github.com>
2023-08-23 21:33:07 -0600

    Add files via upload
    Archivos con los programas que llevaremos a cabo en el examen y necesarios para la calificacion de esta evidencia.

---
+++ 2017064-GABRIEL ALEJANDRO FLORES GUTIERREZ- EDAD- SUMA DE NUMEROS.py
@@ -0,0 +1,7 @@
+#Elabor un programa que sea la suma de dos numeros.
+
+num1 = int(input("Escribe el numero 1 "))
+num2 = int(input("Escribe el numero 2 "))
+vsuma = num1 + num2
+print ("La suma de los numeros es = a",vsuma)
+
---
+++ 2017064-GABRIEL ALEJANDRO FLORES GUTIERREZ- EDAD.py
@@ -0,0 +1,7 @@
+#Crea un programa que te pida tu edad y en base a eso te dira si eres mayor de edad o no 
+
+vEdad = int(input("Dime tu edad "))
+if vEdad>= 18:
+    print ("Eres mayor de edad")
+else:  
+    print ("Eres menor de edad")
